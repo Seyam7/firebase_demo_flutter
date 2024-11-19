f0d4b658a476ea339623b64b5093ab7def6d160c
Sadman Seyam <sadman.seyam07@gmail.com>
2024-11-20 00:19:57 +0600

    firebase setup

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@ import 'package:firebase_crud_flutter/pages/home_page.dart';
 import 'package:flutter/material.dart';
 
 void main() {
+  WidgetsFlutterBinding.ensureInitialized();
   runApp(const MyApp());
 }
 
